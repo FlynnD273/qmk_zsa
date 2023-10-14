139b10f50d5ed02f1e23cd0ccd691c02ffce9011
Flynn Duniho <flynn@harveyosity.org>
2023-10-13 22:54:44 -0400

    set bottom right to tab instead of esc

--- keyboards/moonlander/keymaps/flynn/keymap.c
+++ keyboards/moonlander/keymaps/flynn/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,              KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           LGUI(KC_ESC),                 KC_PGUP,                 KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     LT(L_CAPS,KC_CAPSLOCK),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           LGUI(KC_EQUAL),                 KC_PGDN,                 KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSPO,               KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                                    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSPC,
-    KC_LCTRL,              MO(L_MEDIA),    KC_LALT,        KC_TAB,         KC_ESCAPE,      LGUI(LCTL(KC_C)),                               LT(L_FUNCTION,KC_ESCAPE),                KC_MINUS,       KC_EQUAL,       KC_LBRACKET,    KC_RBRACKET,    KC_RCTL,
+    KC_LCTRL,              MO(L_MEDIA),    KC_LALT,        KC_TAB,         KC_TAB,      LGUI(LCTL(KC_C)),                               LT(L_FUNCTION,KC_ESCAPE),                KC_MINUS,       KC_EQUAL,       KC_LBRACKET,    KC_RBRACKET,    KC_RCTL,
     TD(TD_SPC_DOT),        KC_BSPACE,      KC_LGUI,                                                                        KC_DEL, KC_TAB,         KC_ENTER
   ),
   [L_FUNCTION] = LAYOUT_moonlander(
