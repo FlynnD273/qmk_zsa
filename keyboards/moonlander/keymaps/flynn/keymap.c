f59104f858b1abd9fd6323394283888f6f080d3a
Flynn Duniho <flynn@harveyosity.org>
2023-10-24 22:57:25 -0400

    move magnifier shortcuts to media layer

--- keyboards/moonlander/keymaps/flynn/keymap.c
+++ keyboards/moonlander/keymaps/flynn/keymap.c
@@ -69,8 +69,8 @@ enum tap_dance_codes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_DEFAULT] = LAYOUT_moonlander(
     KC_ESC,                KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_HOME,                        KC_END,                  KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    KC_GRAVE,              KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           LGUI(KC_ESC),                 KC_PGUP,                 KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    LT(L_CAPS,KC_CAPSLOCK),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           LGUI(KC_EQUAL),                 KC_PGDN,                 KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    KC_GRAVE,              KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                 KC_PGUP,                 KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
+    LT(L_CAPS,KC_CAPSLOCK),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                 KC_PGDN,                 KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSPO,               KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                                    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSPC,
     KC_LCTRL,              MO(L_MEDIA),    KC_LALT,        KC_TAB,         KC_LALT,      LGUI(LCTL(KC_C)),                               LT(L_FUNCTION,KC_ESCAPE),                KC_MINUS,       KC_EQUAL,       KC_LBRACKET,    KC_RBRACKET,    KC_RCTL,
     TD(TD_SPC_DOT),        KC_BSPACE,      KC_LGUI,                                                                        KC_DEL, KC_TAB,         KC_ENTER
@@ -108,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_9,       KC_M,      KC_N,                        KC_LALT,        KC_TAB,         KC_ENTER
   ),
   [L_MEDIA] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_R), XX_TERM,     KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F12,
-    KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  LGUI(KC_V),     KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_TAB),   KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                        KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_R), XX_TERM,     LGUI(KC_ESC),                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_HOME,        KC_TRANSPARENT, KC_END,         KC_TRANSPARENT, KC_F12,
+    KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_EQUAL),            KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  LGUI(KC_V),     KC_TRANSPARENT,                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_TAB),   KC_TRANSPARENT,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     LGUI(KC_UP),    KC_DEL, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
